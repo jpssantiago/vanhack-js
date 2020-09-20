32238b486d0ce1b58c3327ae2f9904be126879b9
= <joao_santiago@outlook.com.br>
2020-09-20 20:28:30 -0300

    Layout modification.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const toggleMenu = () => {
     const menu = document.querySelector(".nav-links");
     
-    menu.classList.toggle("nav-active");    
+    
+    menu.classList.toggle("nav-active");
+    menu.style.display = "flex";
 }
