53d693ca3b5d80934d7856951cc765a997841c3c
= <joao_santiago@outlook.com.br>
2020-09-24 14:28:02 -0300

    Fixed responsive layout.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 const toggleMenu = () => {
     const menu = document.querySelector(".nav-links");
-    
-    
+    const editorial = document.querySelector("body > .editorial");
+
     menu.classList.toggle("nav-active");
     menu.style.display = "flex";
+
+    if (editorial.style.display == "none") {
+        editorial.style.displa = "none";
+    } else {
+        editorial.style.display = "block";
+    }
 }
 
 const moveToEvents = () => {
